3a85162fd1e9916dc14958ad57997d2bf7517d66
dr-chaos-2023 <drchaos2020@gmail.com>
2024-01-01 14:31:37 -0300

    Clears all the project

--- app.c
+++ app.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
-#include <assert.h>
-#include "./libs/tests/utils/time/rtc_test.h"
-#include "./libs/tests/utils/time/datetime_test.h"
-
 int main()
 {
-   test_Platform_GetSystemTime();
-   runAllTests();
-   printf("All tests have passed");
+   printf("Hello, World\n");
    return 0;
 }
